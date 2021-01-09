9d3ae980d514bd508f72c0b4b104080cdb61eb4a
Alex Vakhitov <alex.vakhitov@gmail.com>
2021-01-09 10:15:41 +0000

    ugettext_lazy to gettext_lazy deprecation warning (#166)
    * ugettext_lazy to gettext_lazy deprecation warning
    
    * Fixed tests

--- noxfile.py
+++ noxfile.py
@@ -1,193 +1,124 @@
 """Nox sessions."""
-import contextlib
-import shutil
+
 import tempfile
-from pathlib import Path
-from typing import cast
-from typing import Iterator
+from typing import Any
 
 import nox
 from nox.sessions import Session
 
-
-package = "django_sftp"
-python_versions = ["3.8"]
-# nox.options.sessions = "pre-commit", "safety", "mypy", "tests"
-nox.options.sessions = "pre-commit", "safety", "tests"
+# nox.options.sessions = "lint", "safety", "mypy", "tests"
+nox.options.sessions = "lint", "safety", "tests"
 locations = "src", "tests", "noxfile.py", "docs/conf.py"
 
 
-class Poetry:
-    """Helper class for invoking Poetry inside a Nox session.
+def install_with_constraints(session: Session, *args: str, **kwargs: Any) -> None:
+    """Install packages constrained by Poetry's lock file.
 
-    Attributes:
+    This function is a wrapper for nox.sessions.Session.install. It
+    invokes pip to install packages inside of the session's virtualenv.
+    Additionally, pip is passed a constraints file generated from
+    Poetry's lock file, to ensure that the packages are pinned to the
+    versions specified in poetry.lock. This allows you to manage the
+    packages as Poetry development dependencies.
+
+    Arguments:
         session: The Session object.
+        args: Command-line arguments for pip.
+        kwargs: Additional keyword arguments for Session.install.
     """
-
-    def __init__(self, session: Session) -> None:
-        """Constructor."""
-        self.session = session
-
-    @contextlib.contextmanager
-    def export(self, *args: str) -> Iterator[Path]:
-        """Export the lock file to requirements format.
-
-        Args:
-            args: Command-line arguments for ``poetry export``.
-
-        Yields:
-            The path to the requirements file.
-        """
-        with tempfile.TemporaryDirectory() as directory:
-            requirements = Path(directory) / "requirements.txt"
-            self.session.run(
-                "poetry",
-                "export",
-                *args,
-                "--format=requirements.txt",
-                f"--output={requirements}",
-                external=True,
-            )
-            yield requirements
-
-    def version(self) -> str:
-        """Retrieve the package version.
-
-        Returns:
-            The package version.
-        """
-        output = self.session.run(
-            "poetry", "version", external=True, silent=True, stderr=None
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
         )
-        return cast(str, output).split()[1]
-
-    def build(self, *args: str) -> None:
-        """Build the package.
-
-        Args:
-            args: Command-line arguments for ``poetry build``.
-        """
-        self.session.run("poetry", "build", *args, external=True)
+        session.install(f"--constraint={requirements.name}", *args, **kwargs)
 
 
-def install_package(session: Session) -> None:
-    """Build and install the package.
+@nox.session(python="3.8")
+def black(session: Session) -> None:
+    """Run black code formatter."""
+    args = session.posargs or locations
+    install_with_constraints(session, "black")
+    session.run("black", *args)
 
-    Build a wheel from the package, and install it into the virtual environment
-    of the specified Nox session.
 
-    The package requirements are installed using the versions specified in
-    Poetry's lock file.
-
-    Args:
-        session: The Session object.
-    """
-    poetry = Poetry(session)
-
-    with poetry.export() as requirements:
-        session.install(f"--requirement={requirements}")
-
-    poetry.build("--format=wheel")
-
-    version = poetry.version()
-    session.install(
-        "--no-deps", "--force-reinstall", f"dist/{package}-{version}-py3-none-any.whl"
+@nox.session(python=["3.8"])
+def lint(session: Session) -> None:
+    """Lint using flake8."""
+    args = session.posargs or locations
+    install_with_constraints(
+        session,
+        "flake8",
+        "flake8-annotations",
+        "flake8-bandit",
+        "flake8-black",
+        "flake8-bugbear",
+        "flake8-docstrings",
+        "flake8-import-order",
+        "darglint",
     )
-
-
-def install(session: Session, *args: str) -> None:
-    """Install development dependencies into the session's virtual environment.
-
-    This function is a wrapper for nox.sessions.Session.install.
-
-    The packages must be managed as development dependencies in Poetry.
-
-    Args:
-        session: The Session object.
-        args: Command-line arguments for ``pip install``.
-    """
-    poetry = Poetry(session)
-    with poetry.export("--dev") as requirements:
-        session.install(f"--constraint={requirements}", *args)
-
-
-@nox.session(name="pre-commit", python="3.8")
-def precommit(session: Session) -> None:
-    """Lint using pre-commit."""
-    args = session.posargs or ["run", "--all-files", "--show-diff-on-failure"]
-    install(session, "pre-commit")
-    session.run("pre-commit", *args)
+    session.run("flake8", *args)
 
 
 @nox.session(python="3.8")
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
-    poetry = Poetry(session)
-    with poetry.export("--dev", "--without-hashes") as requirements:
-        install(session, "safety")
-        session.run("safety", "check", f"--file={requirements}", "--bare")
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        install_with_constraints(session, "safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
 
 
-@nox.session(python=python_versions)
+@nox.session(python=["3.8"])
 def mypy(session: Session) -> None:
     """Type-check using mypy."""
     args = session.posargs or locations
-    install_package(session)
-    install(session, "mypy")
+    install_with_constraints(session, "mypy")
     session.run("mypy", *args)
 
 
-@nox.session(python=python_versions)
-@nox.parametrize("django", ["3.1"])
-def tests(session: Session, django: str) -> None:
+@nox.session(python=["3.8"])
+def tests(session: Session) -> None:
     """Run the test suite."""
-    args = session.posargs or ["--cov"]
-    install_package(session)
-    install(
+    args = session.posargs or ["--cov", "-m", "not e2e"]
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(
         session,
+        "django",
         "coverage[toml]",
         "pytest",
         "pytest-cov",
         "pytest-django",
         "pytest-pythonpath",
+        "pytest-mock",
     )
-    session.install(f"django=={django}")
     session.run("pytest", *args)
 
 
-@nox.session(python=python_versions)
-def typeguard(session: Session) -> None:
-    """Runtime type checking using Typeguard."""
-    install_package(session)
-    install(session, "pytest", "typeguard")
-    session.run("pytest", f"--typeguard-packages={package}", *session.posargs)
-
-
-@nox.session(python=python_versions)
-def xdoctest(session: Session) -> None:
-    """Run examples with xdoctest."""
-    args = session.posargs or ["all"]
-    install_package(session)
-    install(session, "xdoctest")
-    session.run("python", "-m", "xdoctest", package, *args)
+@nox.session(python="3.8")
+def coverage(session: Session) -> None:
+    """Upload coverage data."""
+    install_with_constraints(session, "coverage[toml]", "codecov")
+    session.run("coverage", "xml", "--fail-under=0")
+    session.run("codecov", *session.posargs)
 
 
 @nox.session(python="3.8")
 def docs(session: Session) -> None:
     """Build the documentation."""
-    args = session.posargs or ["docs", "docs/_build"]
-
-    if session.interactive and not session.posargs:
-        args.insert(0, "--open-browser")
-
-    builddir = Path("docs", "_build")
-    if builddir.exists():
-        shutil.rmtree(builddir)
-
-    install_package(session)
-    install(session, "sphinx", "sphinx-autobuild")
-
-    if session.interactive:
-        session.run("sphinx-autobuild", *args)
-    else:
-        session.run("sphinx-build", *args)
+    session.run("poetry", "install", "--no-dev", external=True)
+    install_with_constraints(session, "sphinx", "sphinx-autodoc-typehints")
+    session.run("sphinx-build", "docs", "docs/_build")
